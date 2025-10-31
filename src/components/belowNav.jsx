d7d8c606518a25c89f5f6ea942dfefdb8a114cfc
milles1234 <dy02082003@gmail.com>
2025-10-31 04:32:32 +0000

    NavBarDone

---
+++ src/components/belowNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BelowNav = () => {
+  return (
+ <div className='min-h-screen bg-gray-100'>
+    <h3>Overview</h3>
+    <p>Breakthrough</p>
+
+    <p>Augen Pro is the first to engineer AI-driven, general-purpose Computing designed</p>
+    <p>Research Insight</p>
+
+ </div>
+  )
+}
+
+export default BelowNav
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='flex flex-col items-center'>
       
-        <ul className='flex space-x-8 mt-4 text-lg font-medium'>
+        <ul className='flex space-x-8 mt-4 text-lg font-medium pt-2 bg-gray-100/80 backdrop-blur-md px-6 py-2 rounded-xl shadow-sm'>
                 {
                     [
                         {label: 'Wearable'},
@@ -12,7 +12,7 @@ const Navbar = () => {
                         {label: 'Programs'},
                         {label: 'Updates'},
                         {label: 'Search'},
-                       
+                      //  <div className="flex items-center gap-8 bg-gray-100/80 backdrop-blur-md px-6 py-2 rounded-xl shadow-sm"></div>
                         
                     ].map(({label}) => (
                         <li key={label}>
@@ -26,19 +26,20 @@ const Navbar = () => {
                     )
                 }
             </ul>
-           <div className='flex absolute inset-x-0 bottom-0 h-50 px-16'>
-            <p>augen</p>
+           <div className='flex absolute inset-x-0 bottom-0 h-50 px-16 '>
+            <p className='font-bold'>augen</p>
               </div>
-            <div className='flex absolute inset-x-0 bottom-0 h-45  gap-3  text-bold px-16'>
-                <h3>
+            <div className='flex absolute inset-x-0 bottom-0 h-45  gap-3  text-bold px-16 pt-1.5'>
+                <h3 className='font-semibold text-2xl'>
               Beyond Humanware
             </h3>
               </div>
-            <div className='flex absolute inset-x-0 bottom-0 h-36  gap-3   text-bold px-16'>
+            <div className='flex absolute inset-x-0 bottom-0 h-36  gap-3   text-bold px-16 pt-4 '>
               <p>Explore</p>
-            <p>A'Sense</p>
-            <p>B'Eye</p>
-            <p>A'Neuro</p>
+           
+            <p className="hover:text-blue-500 transition-colors border-2 border-blue-500 rounded-full w-18 h-7 px-1">A'Sense</p>
+            <p className="hover:text-blue-500 transition-colors border-2 border-blue-500 rounded-full w-18 h-7 px-2.5">B'Eye</p>
+            <p className="hover:text-blue-500 transition-colors border-2 border-blue-500 rounded-full w-18 h-7 px-1.5">A'Neuro</p>
             </div>
             
         
